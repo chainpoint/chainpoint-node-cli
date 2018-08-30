fd0a40d6b8f7514b91fb313965aa79dfe66c8010
Philippe Schommers <philippe@schommers.be>
2018-08-30 02:41:05 +0200

    Fix indentation

--- commands/setup.js
+++ commands/setup.js
@@ -133,9 +133,9 @@ module.exports = {
       spinner.start('Installing Docker Compose')
 
       const command = sudoBash(`
-          mkdir -p /usr/local/bin
-          curl -s -L "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-          chmod +x /usr/local/bin/docker-compose
+        mkdir -p /usr/local/bin
+        curl -s -L "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
       `)
 
       bashSpinner(spinner, command, 'Docker Compose installation')
